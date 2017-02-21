06e223233ea1bc4e7d1f9e0c957f2e584ac19310
Matt Hensley <matt.hensley777@gmail.com>
2017-02-21 16:42:26 -0600

    final commit

--- strings.js
+++ strings.js
@@ -1,28 +1,54 @@
-function reversal(s) {
-	var reverseString = "";
-	for (var i = s.length - 1; i >= 0; i--)
-		reverseString += s[i];
-	return reverseString;
-	document.getElementById("question");
-}
-console.log(reversal("dont press the button"));
+var reverseString = "";
+var question = "";
 
-function alphabits(aOrder) {
-	return aOrder.split("").sort().join("");
-	document.getElementById("question");
+
+
+function reversal() {
+	var question = document.getElementById("question").value;
+	for (var i = question.length - 1; i >= 0; i--)
+		reverseString += question[i];
+	document.getElementById("answer").innerHTML = "<h1>" + reverseString + "</h1>";
+	
 }
-console.log(alphabits("alphabits"));
+console.log("reverseString", reverseString)
+function alphabits() {
+	var question = document.getElementById("question").value;
+	var alphabitize = question.split("").sort().join("");
+	document.getElementById("answer").innerHTML += "<h1>" + alphabitize + "</h1>";
+}
+
 
 function palindrome() {
-
+	var question = document.getElementById("question").value;
+	if (question === reverseString) {
+		document.getElementById("answer").innerHTML += "<h1> Your string is a palindrome </h1>";
+	}
 }
 
-// var button = getElementById("question");
-// console.log("event", clickEvent);
-var testString = "";
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
 
-// document.getElementById("question").addEventListener("click", reversal);
-// document.getElementById("question").addEventListener("click", alphabits);
+document.getElementById("change").addEventListener("click", input)
+
+function input() {
+
+reversal();
+alphabits();
+palindrome();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
